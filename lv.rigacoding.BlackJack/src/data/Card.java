1384ead4bff71f12b450861a4be1301d67fce83b
KristapsAuzans <kristaps.auzans@gmail.com>
2018-01-12 17:40:37 +0200

    Java ALL

---
+++ lv.rigacoding.BlackJack/src/data/Card.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+/**
+ *
+ * @author Kristaps
+ */
+public class Card {
+    // 2,3,.... J,Q, K
+    private String value;
+    //H, S, D, C
+    private char suit;
+    
+    public Card(String value, char suit){
+        this.value = value;
+        this.suit=suit;
+    }
+    
+    public int getValue(){
+        
+    }
+    
+    public String getFullName(){
+        
+    }
+}
