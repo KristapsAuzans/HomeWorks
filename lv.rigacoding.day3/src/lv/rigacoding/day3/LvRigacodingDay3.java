1bc2e24c8f9c595914497fd3a3c7aa55e88fe9a4
KristapsAuzans <kristaps.auzans@gmail.com>
2018-01-12 18:54:50 +0200

    Java ALL

---
+++ lv.rigacoding.day3/src/lv/rigacoding/day3/LvRigacodingDay3.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lv.rigacoding.day3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Kristaps
+ */
+public class LvRigacodingDay3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ievadiet skaitli A:");
+        
+        int a = input.nextInt();
+        
+        System.out.println("Jūsu ievadītā vērtība ir " + a);
+    }
+    
+}
